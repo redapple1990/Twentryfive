fa3958bb4b7e4524ba1137cfcb6c20d9f39880b7
Cool Engineer <55289773+coolenginner@users.noreply.github.com>
2019-12-03 02:45:12 +0300

    update

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,56 +1,22 @@
 <section>
         <!-- Left Sidebar -->
         <aside id="leftsidebar" class="sidebar">
-            <!-- User Info -->
-            <div class="user-info">
-                <div class="image">
-                    <img src="{{url('images',Auth::user()->profiles?Auth::user()->profiles->avatar:'user.png')}}" width="60" height="60" alt="User" />
-                </div>
-                <div class="info-container">
-                    <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{Auth::user()->name}}</div>
-                    <div class="email">{{Auth::user()->email}}</div>
-                    <div class="btn-group user-helper-dropdown">
-                        <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
-                        <ul class="dropdown-menu pull-right">
-                            <li {{ Route::is('profile.index')? 'class=active':'' }}><a href="{{ route('profile.index') }}"><i class="material-icons">person</i>Profile</a></li>
-                            <li role="seperator" class="divider"></li>
-                            <li><a href="javascript:void(0);"><i class="material-icons">group</i>Followers</a></li>
-                            <li><a href="javascript:void(0);"><i class="material-icons">shopping_cart</i>Sales</a></li>
-                            <li><a href="javascript:void(0);"><i class="material-icons">favorite</i>Likes</a></li>
-                            <li role="seperator" class="divider"></li>
-
-
-                            <li>  
-                                <a href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                                        Logout
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <!-- #User Info -->
             <!-- Menu -->
             <div class="menu">
                 <ul class="list">
-                    <li class="header">MAIN NAVIGATION</li>
+                    <li class="header">ARTIST/ACT</li>
                     <li {{Route::is('dashboard')? 'class=active':''}}>
                         <a href="{{route('dashboard')}}">
-                            <i class="material-icons">home</i>
-                            <span>Home</span>
+                            <i class="material-icons">dashboard</i>
+                            <span>Dashboard</span>
                         </a>
                     </li>
 
                     @role('admin') 
                      <li>
                         <a href="javascript:void(0);" class="menu-toggle">
-                            <i class="material-icons">face</i>
-                            <span>Manage Users</span>
+                            <i class="material-icons">event</i>
+                            <span>Events</span>
                         </a>
                         <ul class="ml-menu">                            
                             <li  {{Route::is('permissions.index')||Route::is('permissions.create')||Route::is('permissions.edit')? 'class=active':''}}>
@@ -64,75 +30,18 @@
                             </li>
                         </ul>
                     </li>
-
-                     <li {{Route::is('add-items.index')||Route::is('add-items.create')||Route::is('add-items.edit')? 'class=active':''}}>
-                        <a href="{{route('add-items.index')}}">
-                            <i class="material-icons">exposure</i>
-                            <span>Add Items</span>
-                        </a>
-                    </li>
                     @endrole
 
-                    <!-- <li {{Route::is('typography')? 'class=active':''}}>
+                    <li {{Route::is('typography')? 'class=active':''}}>
                         <a href="{{route('typography')}}">
-                            <i class="material-icons">text_fields</i>
-                            <span>Typography</span>
+                            <i class="material-icons">people</i>
+                            <span>Contacts</span>
                         </a>
                     </li>
                     <li {{Route::is('helper')? 'class=active':''}}>
                         <a href="{{route('form')}}">
-                            <i class="material-icons">build</i>
-                            <span>Form</span>
-                        </a>
-                    </li>
-                    <li {{Route::is('helper')? 'class=active':''}}>
-                        <a href="{{route('helper')}}">
-                            <i class="material-icons">layers</i>
-                            <span>Helper Classes</span>
-                        </a>
-                    </li>
-                    <li {{Route::is('widget')? 'class=active':''}}>
-                        <a href="{{route('widget')}}">
-                            <i class="material-icons">widgets</i>
-                            <span>Widgets</span>
-                        </a>
-                    </li>
-                    <li {{Route::is('table')? 'class=active':''}}>
-                        <a href="{{route('table')}}">
-                            <i class="material-icons">view_list</i>
-                            <span>Tables</span>
-                        </a>
-                    </li>
-                    <li {{Route::is('media')? 'class=active':''}}>
-                        <a href="{{route('media')}}">
-                            <i class="material-icons">perm_media</i>
-                            <span>Medias</span>
-                        </a>
-                    </li>
-                    <li {{Route::is('chart')? 'class=active':''}}>
-                        <a href="{{route('chart')}}">
-                            <i class="material-icons">pie_chart</i>
-                            <span>Charts</span>
-                        </a>
-                    </li> -->
-                    
-                    <li class="header">LABELS</li>
-                    <li>
-                        <a href="javascript:void(0);">
-                            <i class="material-icons col-red">donut_large</i>
-                            <span>Important</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0);">
-                            <i class="material-icons col-amber">donut_large</i>
-                            <span>Warning</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0);">
-                            <i class="material-icons col-light-blue">donut_large</i>
-                            <span>Information</span>
+                            <i class="material-icons">settings</i>
+                            <span>Settings</span>
                         </a>
                     </li>
                 </ul>
@@ -141,7 +50,7 @@
             <!-- Footer -->
             <div class="legal">
                 <div class="copyright">
-                    &copy; 2016 <a href="javascript:void(0);">AdminBSB - Material Design</a>.
+                    &copy; 2019 <a href="javascript:void(0);">Admin dashboard</a>.
                 </div>
                 <div class="version">
                     <b>Version: </b> 1.0.4
