5fd8f3b38c62b50038240c2a8f9ff1abad7bdff7
Cool Engineer <55289773+coolenginner@users.noreply.github.com>
2019-12-02 12:54:44 +0300

    update

--- database/factories/EventFactory.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$factory->define(App\Event::class, function (Faker\Generator $faker) {
-    return [
-        "name" => $faker->name,
-        "event_date" => $faker->date("Y-m-d", $max = 'now'),
-    ];
-});
