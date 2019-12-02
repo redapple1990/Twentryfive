9dfbffa7c6361ee2f4c2a3fdd7909b128e23fdbb
ang-rea-dep <55301985+ang-rea-dep@users.noreply.github.com>
2019-12-02 21:25:54 +0300

    login page update

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,4 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('public/bsbmd/scss/style.scss', 'public/bsbmd/css');
