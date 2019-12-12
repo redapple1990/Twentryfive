ed841eaea191a88f0a0cdfa4707214b99d992eaf
ang-rea-dep <55301985+ang-rea-dep@users.noreply.github.com>
2019-12-12 23:32:32 +0300

    change dashboard page

--- resources/views/layouts/dashboard/index.blade.php
+++ resources/views/layouts/dashboard/index.blade.php
@@ -189,13 +189,5 @@
 @section('extra-script')
     {{Html::script('bsbmd/plugins/jquery-countto/jquery.countTo.js')}}
     {{Html::script('bsbmd/plugins/raphael/raphael.min.js')}}
-    {{Html::script('bsbmd/plugins/morrisjs/morris.js')}}
-    {{Html::script('bsbmd/plugins/chartjs/Chart.bundle.js')}}
-    {{Html::script('bsbmd/plugins/flot-charts/jquery.flot.js')}}
-    {{Html::script('bsbmd/plugins/flot-charts/jquery.flot.resize.js')}}
-    {{Html::script('bsbmd/plugins/flot-charts/jquery.flot.pie.js')}}
-    {{Html::script('bsbmd/plugins/flot-charts/jquery.flot.categories.js')}}
-    {{Html::script('bsbmd/plugins/flot-charts/jquery.flot.time.js')}}
-    {{Html::script('bsbmd/plugins/jquery-sparkline/jquery.sparkline.js')}}
     {{Html::script('bsbmd/js/pages/index.js')}}
 @endsection
