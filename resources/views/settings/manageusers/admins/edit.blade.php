7e574308a9ed1a33cedd5e44dac4dce39facfc14
ang-rea-dep <55301985+ang-rea-dep@users.noreply.github.com>
2019-12-18 02:04:19 +0300

    change edit page in settings

--- resources/views/settings/manageusers/admins/edit.blade.php
+++ resources/views/settings/manageusers/admins/edit.blade.php
@@ -30,17 +30,13 @@
 
 @section('content')
         <div class="container-fluid">
-            <div class="block-header">
-                <h2>Edit Permission</h2>
-            </div>
-
             <!-- Vertical Layout -->
             <div class="row clearfix">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="card">
                         <div class="header">
                             <h2>
-                                Edit Permission
+                                Edit
                             </h2>
                             <ul class="header-dropdown m-r--5">
                                 <li class="dropdown">
@@ -56,17 +52,209 @@
                             </ul>
                         </div>
                         <div class="body">
-                           <form id="form_validation" method="POST" action="{{ route('admins.update',$permission->id) }}">
-                            {{ csrf_field() }}
-                                <div class="form-group form-float">
-                                    <div class="form-line">
-                                        <input name="_method" type="hidden" value="PUT">
-                                        <input type="text" class="form-control" name="name" value="{{ $permission->name }}" required>
-                                        <label class="form-label">Name</label>
+                            <div class="">
+                                <!-- Nav tabs -->
+                                <ul class="nav nav-tabs tab-nav-right" role="tablist">
+                                    <li role="presentation" class="active w-50"><a href="#user-settings" data-toggle="tab" aria-expanded="true">User Settings</a></li>
+                                    <li role="presentation" class="w-50"><a href="#permissions" data-toggle="tab" aria-expanded="false">Permissions</a></li>
+                                </ul>
+        
+                                <!-- Tab panes -->
+                                <div class="tab-content p-t-40">
+                                    <div role="tabpanel" class="tab-pane animated flipInX active" id="user-settings">
+                                        <div class="card margin-0 masked-input">
+                                            <form id="form_validation" method="POST" novalidate="novalidate">
+                                                <div class="header">
+                                                    <div class="row d-flex align-items-center">
+                                                        <div class="col-md-3">
+                                                            <label class="form-label">Photo</label>
+                                                        </div>
+                                                        <div class="col-md-9">
+                                                            <img src="../../../images/user.png" width="60" height="60" alt="User" class="img-circle">
+                                                            <button type="button" class="btn btn-danger m-l-50">Remove</button>
+                                                            <button type="button" class="btn btn-success m-l-20">Update</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="body">
+                                                    <div class="row form-group form-float">
+                                                        <div class="col-md-3">
+                                                            <label class="form-label">First Name</label>
+                                                        </div>
+                                                        <div class="col-md-9">
+                                                            <div class="form-line">   
+                                                                <input type="text" class="form-control" name="Eventname" required="" aria-required="true">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row form-group form-float">
+                                                        <div class="col-md-3">
+                                                            <label class="form-label">Last Name</label>
+                                                        </div>
+                                                        <div class="col-md-9">
+                                                            <div class="form-line">   
+                                                                <input type="text" class="form-control" name="location" required="" aria-required="true">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row form-group form-float">
+                                                        <div class="col-md-3">
+                                                            <label class="form-label">Company</label>
+                                                        </div>
+                                                        <div class="col-md-9">
+                                                            <div class="form-line">   
+                                                                <input type="text" class="form-control" name="location" required="" aria-required="true">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row form-group form-float">
+                                                        <div class="col-md-3">
+                                                            <label class="form-label">Title</label>
+                                                        </div>
+                                                        <div class="col-md-9">
+                                                            <div class="form-line">   
+                                                                <input type="text" class="form-control" name="location" required="" aria-required="true">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row form-group form-float">
+                                                        <div class="col-md-3">
+                                                            <label class="form-label">Birthday</label>
+                                                        </div>
+                                                        <div class="col-md-9">
+                                                            <div class="form-line" id="bs_datepicker_container">   
+                                                                <input type="text" class="form-control" name="location" required="" aria-required="true">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row form-group form-float">
+                                                        <div class="col-md-3">
+                                                            <label class="fo
+                                                            rm-label">Gender</label>
+                                                        </div>
+                                                        <div class="col-md-9">
+                                                            <div class="form-group">
+                                                                <input type="radio" name="Gender" id="unspecified" class="with-gap">
+                                                                <label for="unspecified">Unspecified</label>
+                                    
+                                                                <input type="radio" name="Gender" id="male" class="with-gap">
+                                                                <label for="male" class="m-l-20">male</label>
+                                
+                                                                <input type="radio" name="Gender" id="female" class="with-gap">
+                                                                <label for="female" class="m-l-20">Female</label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row form-group form-float">
+                                                        <div class="col-md-3">
+                                                            <label class="form-label">note</label>
+                                                        </div>
+                                                        <div class="col-md-9">
+                                                            <div class="form-line">   
+                                                                <textarea name="note" id="note" cols="50" rows="3"></textarea>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row form-group form-float">
+                                                        <div class="col-md-3">
+                                                            <label class="form-label">Home</label>
+                                                        </div>
+                                                        <div class="col-md-9">
+                                                            <div class="form-line d-flex align-items-center">
+                                                                <i class="material-icons font-20 m-r-10">email</i>  
+                                                                <input type="text" class="form-control email" name="email" placeholder="Ex: example@example.com">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row form-group form-float">
+                                                        <div class="col-md-3">
+                                                            <label class="form-label">moblie</label>
+                                                        </div>
+                                                        <div class="col-md-9">
+                                                            <div class="form-line d-flex align-items-center">
+                                                                <i class="material-icons font-20 m-r-10">phone_iphone</i>
+                                                                <input type="text" class="form-control mobile-phone-number" placeholder="Ex: +00 (000) 000-00-00">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row form-group form-float">
+                                                        <div class="col-md-3">
+                                                            <label class="form-label">work</label>
+                                                        </div>
+                                                        <div class="col-md-9">
+                                                            <div class="form-line d-flex align-items-center">
+                                                                <i class="material-icons font-20 m-r-10">phone</i>
+                                                                <input type="text" class="form-control mobile-phone-number" name="guestemplete" placeholder="Ex: +00 (000) 000-00-00" required="" aria-required="true">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row form-group form-float">
+                                                        <div class="col-md-3">
+                                                            <label class="form-label">Adress</label>
+                                                        </div>
+                                                        <div class="col-md-9">
+                                                            <div class="form-line d-flex align-items-center">
+                                                                <i class="material-icons font-20 m-r-10">place</i>
+                                                                <input type="text" class="form-control" name="guestemplete" required="" aria-required="true">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="footer">
+                                                    <a href="" class=" d-flex align-items-center">
+                                                        <i class="material-icons m-r-10">add_box</i>
+                                                        Add Field 
+                                                    </a>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        {{-- <form id="form_validation" method="POST" action="{{ route('admins.update',$permission->id) }}">
+                                        {{ csrf_field() }}
+                                            <div class="form-group form-float">
+                                                <div class="form-line">
+                                                    <input name="_method" type="hidden" value="PUT">
+                                                    <input type="text" class="form-control" name="name" value="{{ $permission->name }}" required>
+                                                    <label class="form-label">Name</label>
+                                                </div>
+                                            </div>
+                                            <button class="btn btn-primary waves-effect" type="submit">UPDATE</button>
+                                        </form> --}}
+                                    </div>
+                                    <div role="tabpanel" class="tab-pane animated flipInX" id="permissions">
+                                        <div class="card margin-0 masked-input">
+                                            <form id="form_validation" method="POST" novalidate="novalidate">
+                                                <div class="header">
+                                                    <h2>Permission</h2>
+                                                </div>
+                                                <div class="body">
+                                                    <h2>Users</h2>
+                                                    <div class="d-flex align-items-center">
+                                                        <span class="m-t-10">
+                                                            <input type="checkbox" class="filled-in" id="checkbox1" style="display: none;">
+                                                            <label for="checkbox1"></label>
+                                                        </span>
+                                                        <span>Edit and Delete Users</span>
+                                                    </div>
+                                                    <div class="d-flex align-items-center">
+                                                        <span class="m-t-10">
+                                                            <input type="checkbox" class="filled-in" id="checkbox2" style="display: none;">
+                                                            <label for="checkbox2"></label>
+                                                        </span>
+                                                        <span>Add, Edit, Delete Users Groups</span>
+                                                    </div>
+                                                </div>
+                                                <div class="footer">
+                                                    <a href="" class=" d-flex align-items-center">
+                                                        <i class="material-icons m-r-10">add_box</i>
+                                                        Add Field 
+                                                    </a>
+                                                </div>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
-                                <button class="btn btn-primary waves-effect" type="submit">UPDATE</button>
-                            </form>
+                            </div>
+                           
                         </div>
                     </div>
                 </div>
