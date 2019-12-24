692802468adc206846caf0603763940a27ffe8e1
Cool Engineer <55289773+coolenginner@users.noreply.github.com>
2019-12-25 01:09:07 +0300

    Update index.blade.php

--- resources/views/layouts/dashboard/index.blade.php
+++ resources/views/layouts/dashboard/index.blade.php
@@ -130,34 +130,138 @@
                         <h2>Upcoming Events</h2>
                     </div>
                     <div class="body padding-0">
-                        <!-- <div class="table-responsive">
-                            <table class="table table-hover dashboard-task-infos m-b-0">
-                                <tbody>
-                                    <tr>
-                                        <td class="align-middle text-center"><span>14 March</span></td>
-                                        <td><img src="images/user.png" alt="User" width="60" height="70"></td>
-                                        <td><h4>X SHOW/EVENTS</h4>
-                                            <p>Number of Guests</p>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="align-middle text-center"><span>14 March</span></td>
-                                        <td><img src="images/user.png" alt="User" width="60" height="70"></td>
-                                        <td><h4>X SHOW/EVENTS</h4>
-                                            <p>Number of Guests</p>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="align-middle text-center"><span>14 March</span></td>
-                                        <td><img src="images/user.png" alt="User" width="60" height="70"></td>
-                                        <td><h4>X SHOW/EVENTS</h4>
-                                            <p>Number of Guests</p>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div> -->
-                        
+                            <div>
+                                <div class="tab-content">
+                                    <div role="tabpanel" class="tab-pane fade active in" id="home">
+                                        <div class="panel panel-default panel-post">
+                                            <div class="panel-heading">
+                                                <div class="media">
+                                                    <div class="media-body">
+                                                        <h4 class="media-heading">
+                                                            <a href="https://lcafood2020.com/">13-16 Oct 2020, LCA Food 2020, Berlin, Germany (hosted by DIL)</a>
+                                                        </h4>
+                                                        350 members - 16 Oct 2020
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="panel-body">
+                                                <div class="post">
+                                                    <div class="post-content">
+                                                        <img src="https://www.mypackfood.eu/wp-content/uploads/2019/11/lyfecycle.png" class="img-responsive" width="100%" height="360">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="panel panel-default panel-post">
+                                            <div class="panel-heading">
+                                                <div class="media">
+                                                    <div class="media-body">
+                                                        <h4 class="media-heading">
+                                                            <a href="https://operaandballet.com/month/2020-december/?sid=GLE_1&play_theatre=237&play_perfomance=3645&play_date_from=01-Dec-2020&play_date_to=31-Dec-2020">Tchaikovsky Concert Hall for 01 December 2020 - 31 December 2020</a>
+                                                        </h4>
+                                                        1003 members - 01 Dec 2020
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="panel-body">
+                                                <div class="post">
+                                                    <div class="post-content">
+                                                        <iframe width="100%" height="556" src="https://www.youtube.com/embed/4I2ApQITQT8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div role="tabpanel" class="tab-pane fade" id="profile_settings">
+                                        <form class="form-horizontal">
+                                            <div class="form-group">
+                                                <label for="NameSurname" class="col-sm-2 control-label">Name Surname</label>
+                                                <div class="col-sm-10">
+                                                    <div class="form-line focused">
+                                                        <input type="text" class="form-control" id="NameSurname" name="NameSurname" placeholder="Name Surname" value="Marc K. Hammond" required="">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="Email" class="col-sm-2 control-label">Email</label>
+                                                <div class="col-sm-10">
+                                                    <div class="form-line focused">
+                                                        <input type="email" class="form-control" id="Email" name="Email" placeholder="Email" value="example@example.com" required="">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="InputExperience" class="col-sm-2 control-label">Experience</label>
+
+                                                <div class="col-sm-10">
+                                                    <div class="form-line">
+                                                        <textarea class="form-control" id="InputExperience" name="InputExperience" rows="3" placeholder="Experience"></textarea>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="InputSkills" class="col-sm-2 control-label">Skills</label>
+
+                                                <div class="col-sm-10">
+                                                    <div class="form-line">
+                                                        <input type="text" class="form-control" id="InputSkills" name="InputSkills" placeholder="Skills">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <div class="col-sm-offset-2 col-sm-10">
+                                                    <input type="checkbox" id="terms_condition_check" class="chk-col-red filled-in">
+                                                    <label for="terms_condition_check">I agree to the <a href="#">terms and conditions</a></label>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="col-sm-offset-2 col-sm-10">
+                                                    <button type="submit" class="btn btn-danger">SUBMIT</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div role="tabpanel" class="tab-pane fade" id="change_password_settings">
+                                        <form class="form-horizontal">
+                                            <div class="form-group">
+                                                <label for="OldPassword" class="col-sm-3 control-label">Old Password</label>
+                                                <div class="col-sm-9">
+                                                    <div class="form-line">
+                                                        <input type="password" class="form-control" id="OldPassword" name="OldPassword" placeholder="Old Password" required="">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="NewPassword" class="col-sm-3 control-label">New Password</label>
+                                                <div class="col-sm-9">
+                                                    <div class="form-line">
+                                                        <input type="password" class="form-control" id="NewPassword" name="NewPassword" placeholder="New Password" required="">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="NewPasswordConfirm" class="col-sm-3 control-label">New Password (Confirm)</label>
+                                                <div class="col-sm-9">
+                                                    <div class="form-line">
+                                                        <input type="password" class="form-control" id="NewPasswordConfirm" name="NewPasswordConfirm" placeholder="New Password (Confirm)" required="">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <div class="col-sm-offset-3 col-sm-9">
+                                                    <button type="submit" class="btn btn-danger">SUBMIT</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        <!-- </div>
+                    </div> -->
+                <!-- </div> -->
                     </div>
                 </div>
             </div>
